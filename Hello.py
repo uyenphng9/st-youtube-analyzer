95db29a7e98cfee95bf50a04a46381d0c6a42f31
Ngoc Uyen Phung <148657580+uyenphng9@users.noreply.github.com>
2023-12-19 08:21:35 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,143 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+"""
+This streamlit application allows users to analyse and visualise 
+youtube channels data. 
+"""
 
 import streamlit as st
-from streamlit.logger import get_logger
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
 
-LOGGER = get_logger(__name__)
-
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+st.title('The Youtube Channels Analysis 🎬')
+SIDEBAR_TITLE = 'Navigation'
+MENU_MSG = 'Choose your action'
+MENU_CHOICES = ['Data explorer', 'Data analysis']
 
 
-if __name__ == "__main__":
-    run()
+@st.cache_data
+def get_category(data):
+    """Get a list of unique categories from the dataset."""
+    category_list = data['category'].unique().tolist()
+    sort_category = sorted(map(str, category_list))
+    return sort_category[:10]
+
+
+@st.cache_data
+def get_channel(data):
+    """Get a count of YouTubers for each category."""
+    return data.groupby('category').count()['Youtuber'].reset_index()
+
+
+@st.cache_data
+def load_data():
+    """Load and preprocess the YouTube data from the CSV file."""
+    df_youtube = pd.read_csv('most_subscribed_youtube_channels.csv')
+
+    # Change the data type
+    df_youtube["subscribers"] = [
+        value.replace(",", "") for value in df_youtube["subscribers"]]
+    df_youtube["video views"] = [
+        value.replace(",", "") for value in df_youtube["video views"]]
+    df_youtube["subscribers"] = df_youtube["subscribers"].astype("float")
+    df_youtube["video views"] = df_youtube["video views"].astype("float")
+
+    return df_youtube
+
+
+def data_explorer(data):
+    """Explore and visualize the YouTube data."""
+    st.header('Data Explorer')
+    category_list = ['All'] + get_category(data)
+    selected_category = st.selectbox('Select the category', category_list)
+
+    # Show filtered data
+    if selected_category == 'All':
+        st.write('The data you want to see')
+        st.write(data.head())
+        filtered_data = data
+    else:
+        st.write(f'You have selected {selected_category}')
+        filtered_data = data[data['category'] == selected_category]
+        st.write(filtered_data)
+
+    # Create a download button for the filtered data
+    if st.button("Download the data"):
+        csv = filtered_data.to_csv(index=False).encode('utf-8')
+        st.download_button(
+            label="Download as CSV",
+            data=csv,
+            file_name='filtered_data.csv',
+            mime='text/csv',
+        )
+
+    # Create a bar plot for the number of YouTubers in each category
+    st.header('Category Distribution')
+    st.subheader('You can see the most popular category here')
+    category_counts = data['category'].value_counts().reset_index()
+    category_counts.columns = ['Category', 'Count']
+    fig = px.bar(category_counts, x='Category', y='Count',
+                 title='Number of YouTubers in Each Category')
+    st.plotly_chart(fig)
+
+
+def plot_ranking(data, ranking_by):
+    fig2, _ = plt.subplots()
+
+    # Sort the data based on the selected ranking metric
+    data = data.sort_values(by=ranking_by, ascending=False)
+
+    # Create a bar plot, presents top 10
+    sns.barplot(data=data[:10], x='Youtuber', y=ranking_by)
+    plt.xlabel('Youtuber')
+    plt.ylabel(f"Number of {ranking_by}")
+    plt.title(f"Top 10 YouTubers ranking by {ranking_by}")
+    plt.xticks(rotation=45, ha="right")
+    st.pyplot(fig2)
+
+
+def data_analysis(data):
+    st.header('Data Analysis')
+    col1, col2 = st.columns(2)
+
+    # Column 1: based on ranking metrics
+    with col1:
+        st.subheader(
+            'See the most popular YouTuber based on different ranking metrics')
+        ranking_metric = st.selectbox('Select a ranking metric', [
+            'subscribers', 'video views'])
+        st.write(f"Ranking based on: {ranking_metric}")
+        sorted_data = data.sort_values(by=ranking_metric, ascending=False)
+        st.write(
+            sorted_data[['Youtuber', ranking_metric]].reset_index(drop=True))
+
+    # Column 2: ranking plot
+    with col2:
+        plot_ranking(data, ranking_metric)
+
+    # Scatter plot
+    st.subheader('Scatter Plot: Subscribers vs. Video Views')
+    fig, ax = plt.subplots()
+    ax.scatter(data['subscribers'], data['video views'], alpha=0.5)
+    ax.set_xlabel('Subscribers')
+    ax.set_ylabel('Video Views')
+    st.pyplot(fig)
+
+
+def main():
+    df_youtube = load_data()
+
+    st.sidebar.title(SIDEBAR_TITLE)
+
+    menu = st.sidebar.selectbox(MENU_MSG, MENU_CHOICES)
+
+    if menu == 'Data explorer':
+        data_explorer(df_youtube)
+    elif menu == 'Data analysis':
+        data_analysis(df_youtube)
+    else:
+        st.write('Error: Invalid selection')
+
+
+if __name__ == '__main__':
+    main()
